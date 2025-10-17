f32ecbef737d1b2e2ed34e1f308a0afd4c070c27
r4yb3l <108087778+r4yb3l@users.noreply.github.com>
2025-10-17 07:26:14 -0600

    Remove redundant comments in CSS and i18n files for cleaner code.

--- portfolio/src/i18n/index.ts
+++ portfolio/src/i18n/index.ts
@@ -1,8 +1,6 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
-
-// Importar traducciones
 import es from './locales/es.json'
 import en from './locales/en.json'
 
